bd30744852bf518743d30bf9e4a16133632fa276
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-11 11:01:52 +0300

    fix: update entities

--- src/main/java/arden/java/islab1/model/user/Role.java
+++ src/main/java/arden/java/islab1/model/user/Role.java
@@ -2,14 +2,12 @@ package arden.java.islab1.model.user;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
-import java.util.HashSet;
-import java.util.Set;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "is_lab1_roles")
 @Data
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Short id;
@@ -17,6 +15,8 @@ public class Role {
     @Column(unique = true, nullable = false)
     private String role;
 
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users = new HashSet<>();
+    @Override
+    public String getAuthority() {
+        return role;
+    }
 }
--- src/main/java/arden/java/islab1/model/user/User.java
+++ src/main/java/arden/java/islab1/model/user/User.java
@@ -2,22 +2,32 @@ package arden.java.islab1.model.user;
 
 import arden.java.islab1.model.vehicle.Vehicle;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(name = "is_lab1_app_user")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
+    @Size(min = 5, max = 64, message = "Username can have from 5 to 64 chars")
+    @NotBlank(message = "Username can't be blank")
     private String username;
+
     @Column(unique = true)
+    @Size(min = 5, max = 128, message = "Password can have from 5 to 128 chars")
+    @NotBlank(message = "Password can't be blank")
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
@@ -30,4 +40,29 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Vehicle> vehicles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
 }
